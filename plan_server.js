e56c5517136432fbdbcfdf45049c5893b7711e24
liRemons <remons@foxmail.com>
2023-06-22 14:36:41 +0800

    feat: router history

--- plan_server.js
+++ plan_server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 const port = 8008;
 app.use(express.static("dist", { maxAge: 1000 * 3600 }));
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './dist/index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'))
 })
 app.listen(port, () => console.log(`Example app listening on port port!`));
